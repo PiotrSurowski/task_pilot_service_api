a9762fd16aacdb4ef33e3264beaedbef9c6277bd
Piotr Surowski <piotr.surowski248@gmail.com>
2025-04-26 19:17:00 +0200

    init

---
+++ src/main/java/pl/wsei/task_pilot_service_api/TaskPilotServiceApiApplication.java
@@ -0,0 +1,13 @@
+package pl.wsei.task_pilot_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPilotServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPilotServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wsei/task_pilot_service_api/TaskPilotServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsei.task_pilot_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPilotServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
