58a4dc87cfe0d80e7ac34ef9ffeda0e0b2205e59
Piotr Surowski <piotr.surowski248@gmail.com>
2025-06-08 17:07:41 +0200

    Zmiana uprawnien dla task

--- application/src/main/java/pl/wsei/config/SecurityConfiguration.java
+++ application/src/main/java/pl/wsei/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package pl.wsei.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -54,7 +55,9 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
-                                .requestMatchers("/api/task/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/api/task/create").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/task/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/task/performer/**").hasAnyRole("USER", "ADMIN")
                                 .requestMatchers("/api/user/getAll").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
                                 .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
